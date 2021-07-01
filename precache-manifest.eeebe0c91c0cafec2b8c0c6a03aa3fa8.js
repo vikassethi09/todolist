912f549edd44d1e2dde6fd1b231a68961ae8b4c8
Vikas Sethi <vikassethi2000@gmail.com>
2021-07-01 10:21:20 +0530

    Updates

---
+++ precache-manifest.eeebe0c91c0cafec2b8c0c6a03aa3fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792d7306d128e041889f218d3bf5051a",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "fe1e687b0d2670d3f9a1",
+    "url": "/todolist/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/todolist/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1e687b0d2670d3f9a1",
+    "url": "/todolist/static/js/main.c71f0399.chunk.js"
+  },
+  {
+    "revision": "8c4d5725f9902603cd5c",
+    "url": "/todolist/static/js/runtime-main.13c84a0a.js"
+  }
+]);
